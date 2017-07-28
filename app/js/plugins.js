be59f19bb3cf255992752ef1ccafb6c28d6bd5e0
Karine91 <kitana.91@mail.ru>
2017-07-28 20:15:06 +0300

    slider almost done

--- app/js/plugins.js
+++ app/js/plugins.js
@@ -1,16 +1,29 @@
-function centeredBottomElement(container, element){
-    var containerHeight = container.outerHeight();
-    var containerWidth = container.outerWidth();
-    var elementHeight = element.outerHeight(true)+21;
-    var positionElementTextTop = containerHeight-35-elementHeight;
-    var positionElementTextLeft = containerWidth/2;
-    var elementWidth = element.outerWidth();
-    positionElementTextLeft -= elementWidth/2;
+var windowHeight = $(window).outerHeight();
+var windowWidth = $(window).width();
+var contentContainerWidth = 1170;
+var contentContainerLeft = (windowWidth - contentContainerWidth)/2;
+var contentContainerRight = contentContainerLeft + contentContainerWidth;
+function centeredBottomElement(element){
+
+    var positionElementTextTop = windowHeight-(element.outerHeight()+21)/2-35;
+    var positionElementTextLeft = windowWidth/2;
     var position = positionElementTextTop+","+positionElementTextLeft;
     element.attr('data-position',position);
+    centered(element);
 }
 
-
+function centered(element){
+    var elementWidthHalf = element.outerWidth()/2;
+    var elementHeightHalf = (element.outerHeight()+21)/2;
+    element.css('margin-left', "-"+elementWidthHalf+"px");
+    element.css('margin-top', "-"+elementHeightHalf+"px");
+}
+function percentToPixelVert(val){
+    return val/100*windowHeight;
+}
+function percentToPixelHoryz(val){
+    return val/100*windowWidth;
+}
 jQuery(window).load(function(){
     //Slider
     //Посчитаем позицию нижнего блока первый слайд
@@ -20,31 +33,52 @@ jQuery(window).load(function(){
 
 
     $(window).resize(function(){
-        var windowHeight = $(window).outerHeight();
-        var windowWidth = $(window).width();
-        console.log(windowWidth);
-        console.log(windowHeight);
+        centered(slideElement);
     });
-    var windowHeight = $(window).outerHeight();
-    var windowWidth = $(window).width();
-    var dimensions = windowWidth +"," + windowHeight;
-    var fishPosition = windowHeight*0.45 +","+ windowWidth*1.2;
 
-
+    ///SLIDE 1
+    var fishPosition = percentToPixelVert(50) +","+ percentToPixelHoryz(101);
     var elementFish = $('.slider_block__image_fish');
     elementFish.attr('data-position',fishPosition);
+    centeredBottomElement(slideElement);
+    ///SLIDE 2
 
 
-    centeredBottomElement(slider,slideElement);
+    var textItems = $('.slider_block__text_item');
+    var positionTop= percentToPixelVert(50);
+    textItems.each(function(){
+        var that = $(this);
+        var itemWidth = that.outerWidth();
+        var itemHeight = that.outerHeight();
+        var positionLeft = contentContainerRight -itemWidth;
+        var position = positionTop+","+ positionLeft;
+        that.attr('data-position',position);
+        positionTop +=10+itemHeight;
+    });
+
+
+    ///SLIDE 3
+
+    var fish1_slide3 = $('.slider_block__image_fish1-slide3');
+    fishPosition = percentToPixelVert(50) +","+ percentToPixelHoryz(101);
+    fish1_slide3.attr('data-position',fishPosition);
+
+    var fish2_slide3 = $('.slider_block__image_fish2-slide3');
+    fishPosition = percentToPixelVert(10) +","+ percentToPixelHoryz(-40);
+    fish2_slide3.attr('data-position',fishPosition);
+
+    var fish3_slide3 = $('.slider_block__image_fish3-slide3');
+    fishPosition = percentToPixelVert(90) +","+ percentToPixelHoryz(-40);
+    fish3_slide3.attr('data-position',fishPosition);
 
     //Init slider
     slider.fractionSlider({
         'fullWidth': 			true,
         'controls': 			true,
         'pager': 				false,
-        'slideEndAnimation':    true,
-        'responsive':  			true,
-        'dimensions':  			dimensions
+        'slideEndAnimation':    true
+        // 'responsive':  			true,
+        // 'dimensions':  			containerWidth+","+containerHeight
 
     });
 
