83c6585e848eb5a1ba227a4542aebf85d8655845
Karine91 <kitana.91@mail.ru>
2017-08-05 12:59:28 +0300

    future-events done

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,17 @@
 $(function(){
+    $(window).bind('mousewheel DOMMouseScroll MozMousePixelScroll', function(event){
+        if($(document).scrollTop() ==$('.gallery-shark-wrap').offset().top -$(window).innerHeight()){
+           setTimeout(function(){
+               $('.gallery-shark__text .section_header').animate({
+                   opacity: "1"
+               },2500,"easeOutQuart", function(){
+                   $('.gallery-shark__btn').animate({
+                       opacity: "1"
+                   },1000);
+               });
+           }, 1000);
+        }
+    });
 
 
 
